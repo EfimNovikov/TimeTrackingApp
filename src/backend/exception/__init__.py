0a4eef89166a8940920ad59286b4b1c346df9023
Efim Novikov-Glushkov <sawtery@gmail.com>
2023-08-31 12:53:01 +0300

    restructured project file

--- src/backend/exception/__init__.py
+++
@@ -1,5 +0,0 @@
-class CategoryNotFoundException(Exception):
-    """Custom exception to indicate that a category with the provided name was not found."""
-
-class TaskNotFoundException(Exception):
-    """Custom exception to indicate that a task with the provided name was not found."""
